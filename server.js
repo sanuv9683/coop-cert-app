646b136192fe36363908d6464af783bd1aa3b599
sanuv9683 <sanuv9683@gmail.com>
2025-04-28 09:08:23 +0100

    initiate the updates

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const bcrypt  = require('bcrypt');
+const bcrypt  = require('bcryptjs');
 const session = require('express-session');
 const path    = require('path');
 
